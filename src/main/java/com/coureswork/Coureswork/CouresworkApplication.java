81a3bba690311ec7cde86cfdd6b430b25444a4a5
Danindu-Matheesha <danindu.matheesha2004@gmail.com>
2024-09-30 11:35:53 +0530

    Spring Boot Setup

---
+++ src/main/java/com/coureswork/Coureswork/CouresworkApplication.java
@@ -0,0 +1,13 @@
+package com.coureswork.Coureswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouresworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouresworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coureswork/Coureswork/CouresworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coureswork.Coureswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouresworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
